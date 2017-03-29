f115f5614da6f49ba3bf5b1f2a4bc385467ec6c7
sunya9 <mugen.xyz@gmail.com>
2017-03-29 18:46:51 +0900

    Refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,9 @@
-// import 'font-awesome-webpack!../../font-awesome.config.js'
-// import 'font-awesome-webpack'
+import 'whatwg-fetch'
+import 'promise-polyfill'
 import '../css/main.css'
 import 'font-awesome/css/font-awesome.css'
 
-const { pathname } = location
-let path = pathname
+let path = location.pathname
 if(path === '/') path = '/index'
-if(path[path.length -1] === '/')
-  path = path.substr(0, path.length -1)
-
-const whitelist = [
-  '/repositories'
-]
-const { __resource__ : resource } = window
-
-let idx = -1
-// FIXME
-for(let i = 0; whitelist.length > i; i++) {
-  idx = path.indexOf(whitelist[i])
-}
-if(idx >= 0) {
-  path = whitelist[idx]
-  import(`.${path}/${resource}`).catch(err => console.error.bind(console))
-}
+import(`.${path}`)
+  .catch(err => console.error.bind(console))
