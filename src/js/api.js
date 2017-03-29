a04caccb05783bbcdf9a755b43241b37e6fff366
sunya9 <mugen.xyz@gmail.com>
2017-03-29 18:44:53 +0900

    Simplify views

--- src/js/api.js
+++
@@ -1,9 +0,0 @@
-import 'whatwg-fetch'
-
-export default {
-  getRepositories() {
-    return fetch('/repositories/', {
-      credentials: 'include'
-    }).then(res => res.json())
-  }
-}
--- src/js/show.js
+++ src/js/show.js
@@ -4,13 +4,20 @@ new Vue({
   el: '#branches',
   data: {
     branches: window.__branches__,
-    displayAll: false
+    displayAll: false,
+    owner: window.__owner__,
+    repo: window.__repo__
   },
   computed: {
     showBranches() {
       return this.displayAll
         ? this.branches
         : this.branches.filter(branch => branch.docker.exist)
+    }
+  },
+  methods: {
+    previewURL(name, id) {
+      return `/preview?repo=${this.repo}&owner=${this.owner}&branch=${name}&commit_id=${id.substr(0, 6)}`
     }
   }
 })
