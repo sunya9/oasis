4ff812a4e9919b8fcc40cc8bb587d6d2f071bfff
sunya9 <mugen.xyz@gmail.com>
2017-03-30 15:28:56 +0900

    Organize routing

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,56 @@
 import Vue from 'vue'
+import moment from 'moment'
 
-new Vue({
-  el: '#repos',
-  data: {
-    repos: window.__repos__,
-    filterText: ''
-  },
-  computed: {
-    filterRepos() {
-      if(!this.filterText) return this.repos
-      return this.repos.filter(repository => {
-        return repository.full_name.indexOf(this.filterText) >= 0
+(() => {
+  if(!document.getElementById('repos')) return
+  new Vue({
+    el: '#repos',
+    data: {
+      filterText: '',
+      page: 1,
+      end: false,
+      repos: [],
+      promise: null
+    },
+    computed: {
+      filterRepos() {
+        if(!this.filterText) return this.repos
+        return this.repos.filter(repository => {
+          return repository.full_name.indexOf(this.filterText) >= 0
+        })
+      }
+    },
+    mounted() {
+      window.addEventListener('scroll', () => {
+        const { body } = document
+        if ((body.scrollTop + window.innerHeight) >= body.scrollHeight) {
+          if(!this.promise && !this.end)
+            this.get()
+        }
       })
+      this.get()
+    },
+    methods: {
+      get() {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.promise = fetch(`/?page=${this.page++}`, {
+          headers,
+          credentials: 'include'
+        })
+          .then(res => res.json())
+          .then(res => {
+            this.repos = this.repos.concat(res.data)
+            this.end = !res.has_next
+            this.promise = null
+          })
+      },
+      relative(date) {
+        return moment(date).fromNow()
+      },
+      absolute(date) {
+        return moment(date).format('YYYY/MM/DD hh:mm:ss')
+      }
     }
-  }
-})
+  })
+})()
--- src/js/show.js
+++ src/js/show.js
@@ -1,23 +1,60 @@
 import Vue from 'vue'
+import qs from 'querystring'
 
 new Vue({
   el: '#branches',
   data: {
-    branches: window.__branches__,
-    displayAll: false,
-    owner: window.__owner__,
-    repo: window.__repo__
+    branches: [],
+    commits: [],
+    displayAll: true,
+    owner: null,
+    repo: null,
+    branch: '',
+    promise: null
   },
   computed: {
-    showBranches() {
+    showCommits() {
       return this.displayAll
-        ? this.branches
-        : this.branches.filter(branch => branch.docker.exist)
+        ? this.commits
+        : this.commits.filter(commit => commit.docker.exist)
     }
+  },
+  watch: {
+    branch() {
+      this.getCommits()
+    }
+  },
+  mounted() {
+    const { owner, repo } = qs.parse(location.search.substr(1))
+    this.owner = owner
+    this.repo = repo
+    fetch(`/branch${location.search}`, {
+      credentials: 'include'
+    })
+      .then(res => res.json())
+      .then(res => {
+        this.branches = res
+        if(res.length > 0) {
+          this.branch = res[0].name
+        }
+      })
   },
   methods: {
     previewURL(name, id) {
       return `/preview?repo=${this.repo}&owner=${this.owner}&branch=${name}&commit_id=${id.substr(0, 6)}`
+    },
+    getCommits() {
+      this.commits = []
+      const query = qs.parse(location.search.substr(1))
+      query.branch = this.branch
+      this.promise = fetch(`/commit?${qs.stringify(query)}`, {
+        credentials: 'include'
+      })
+        .then(res => res.json())
+        .then(res => {
+          this.commits = res
+          this.promise = null
+        })
     }
   }
 })
